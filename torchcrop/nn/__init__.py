6cc39cea5cc74904e46ae1d5e98050cd1c7ef1e3
geonextgis <geonextgis@gmail.com>
2026-05-29 20:01:54 +0200

    Update init

--- torchcrop/nn/__init__.py
+++ torchcrop/nn/__init__.py
@@ -1,7 +1,21 @@
 """ML components for hybrid crop modeling."""
 
+from torchcrop.nn.hybrid import (
+    HybridManager,
+    ResidualHead,
+    ResidualSpec,
+    default_slots,
+)
 from torchcrop.nn.learned_stress import LearnedStressFactor
 from torchcrop.nn.param_net import ParameterNet
 from torchcrop.nn.residual import NeuralResidual
 
-__all__ = ["LearnedStressFactor", "NeuralResidual", "ParameterNet"]
+__all__ = [
+    "HybridManager",
+    "LearnedStressFactor",
+    "NeuralResidual",
+    "ParameterNet",
+    "ResidualHead",
+    "ResidualSpec",
+    "default_slots",
+]
